33df2863c226735e47d9fe47a3dc49bb206561a3
Jaiwanth <jaiwanth2011@gmail.com>
2020-07-05 20:51:22 +0530

    Updates

---
+++ precache-manifest.7a1d2a9639e2cf88e98986caa7fafa23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690627bb5ae59cbcb772e3f7e6f1768b",
+    "url": "/LightsOut/index.html"
+  },
+  {
+    "revision": "fcaa4488270efaa93603",
+    "url": "/LightsOut/static/css/main.09dd99c2.chunk.css"
+  },
+  {
+    "revision": "7339c324199ea1b33a5e",
+    "url": "/LightsOut/static/js/2.12af845b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LightsOut/static/js/2.12af845b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcaa4488270efaa93603",
+    "url": "/LightsOut/static/js/main.9184f79d.chunk.js"
+  },
+  {
+    "revision": "8229a34c60e3271d0d6b",
+    "url": "/LightsOut/static/js/runtime-main.4d3906ec.js"
+  }
+]);
