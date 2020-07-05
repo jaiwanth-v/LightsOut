325c8de39cb4e570fe16a09acd6dbe01c8f0a6ec
Jaiwanth <jaiwanth2011@gmail.com>
2020-07-05 20:32:57 +0530

    Deploy

---
+++ src/Cell.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import "./Cell.css"
+
+
+
+export default class Cell extends Component {
+  handleClick = () => {
+    this.props.flipCellsAroundMe();
+  }
+
+  render() {
+    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
+    return (
+        <td className={classes} onClick={this.handleClick} />
+    )
+  }
+}
+
+
